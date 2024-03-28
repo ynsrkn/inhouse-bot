df6a78cde6c04d430e07a9b59d81ba49ed133875
ynsrkn <163357433+ynsrkn@users.noreply.github.com>
2024-03-28 18:41:11 -0400

    Improved match scraping (#8)

--- scrape_match_data.py
+++ scrape_match_data.py
@@ -3,7 +3,7 @@ import os
 import json
 import urllib3
 import logging
-import sys
+from lcu_connector import Connector
 
 # shut up let me write bad code
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
@@ -15,40 +15,49 @@ URL_BASE = "https://127.0.0.1:"
 MATCHES_ENDPOINT = "/lol-match-history/v1/products/lol/current-summoner/matches"
 GAMES_ENDPOINT = "/lol-match-history/v1/games/"
 
-if __name__ == "__main__":
-    port = sys.argv[1]
-    auth = sys.argv[2]
+class ClientNotOpenException(Exception):
+    pass
 
-    headers = {
-        'accept': 'application/json',
-        'Authorization': 'Basic ' + auth,
-    }
+def scrape_match_data() -> None:
+    try:
+        connector = Connector(start=True)
+    except:
+        logging.warning("Client closed, cannot scrape new games")
+        raise ClientNotOpenException
+
+    logging.info("Getting local match history")
+    
+    headers = connector.headers
 
     # get list of matches
-    response = requests.get(
-        URL_BASE + port + MATCHES_ENDPOINT,
-        headers=headers,
-        verify=False,
-    )
+    response = requests.get(connector.url + MATCHES_ENDPOINT, headers=headers, verify=False)
     riotGameIds = []
     # get riotGameIds of inhouse custom games
     matches = response.json()['games']['games']
     for match in matches:
         if match['gameMode'] == 'CLASSIC' and match['gameType'] == 'CUSTOM_GAME' and match['endOfGameResult'] == 'GameComplete':
             riotGameIds.append(match['gameId'])
-    
+
     # get each match data
+    retrieved_game = False
     for i, riotGameId in enumerate(riotGameIds[::-1]):
         matchId = len(os.listdir("matches/")) + 1
         matchFileName = f"matches/match-{riotGameId}.json"
         if not os.path.exists(matchFileName):
             logging.info(f"Getting data for riotGameId: {riotGameId}, matchId: {matchId}")
             response = requests.get(
-                URL_BASE + port + GAMES_ENDPOINT + str(riotGameId),
+                connector.url + GAMES_ENDPOINT + str(riotGameId),
                 headers=headers,
                 verify=False
             )
+            retrieved_game = True
             
             match = json.dumps(response.json(), indent=4)
             with open(matchFileName, 'w') as matchFile:
                 matchFile.write(match)
+    
+    if not retrieved_game:
+        logging.info("No new games")
+
+if __name__ == "__main__":
+    scrape_match_data()
